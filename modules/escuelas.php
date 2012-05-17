b224a21581f4422a0aa8abf28930036bc263f8fe
Jesus Christian Cruz Acono <jccruz@openintelligence.mx>
2012-05-17 02:30:34 -0500

    create a escuelas prototype

---
+++ modules/escuelas.php
@@ -0,0 +1,12 @@
+<?php
+class action extends actions{
+	public function index(){
+		return array();
+	}
+	public function add(){
+		return array(array("name" -> 25));
+	}
+	public function get($id){
+		return array(array("name" -> 25));
+	}
+}
