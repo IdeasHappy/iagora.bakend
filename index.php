60ecaabd0e84627fd359daaa29341563a133e7c6
Jesus Christian Cruz Acono <jccruz@openintelligence.mx>
2012-05-17 04:01:19 -0500

    changes to render

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $variableMetodo = array($action1, $route->getAction());
 var_dump(is_callable($variableMetodo, false, $nombre_a_llamar));  //  bool(true)
 
 echo $nombre_a_llamar, "\n"; 
-call_user_func("$action1->" . $route->getAction());
+call_user_func(array($action1, 'index'));
 $render->setModule($route->getModule());
 $render->setAction($route->getAction());
 print_r($action->getValues());
