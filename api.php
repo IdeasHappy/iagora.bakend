f742500616b0136590d1f795ed707118da324a9c
Jesus Christian Cruz Acono <jccruz@openintelligence.mx>
2012-05-17 04:06:57 -0500

    created api manager

--- api.php
+++ api.php
@@ -6,7 +6,6 @@ require_once("./route.php");
 $route = new route($_GET);
 require_once("./modules/". $route->getModule() . ".php");
 $action1 = new action($route->getParams());
-$render->load();
 call_user_func(array($action1, 'index'));
 echo json_encode($action1->getValues());
 
