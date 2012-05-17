c44910f6f0f7f5296ed6f915b4c2c6d4692d11b8
Jesus Christian Cruz Acono <jccruz@openintelligence.mx>
2012-05-17 02:38:13 -0500

    added code to the render

--- lib/render.php
+++ lib/render.php
@@ -4,10 +4,13 @@ class render{
 
 	}
 	public function load(){
-
+		$header = new coSimpleTemplate("./tpl/header.tpl");
+		$footer = new coSimpleTemplate("./tpl/footer.tpl");
 	}
 	public function setAction($array = array()){
-
+		$index = new coSimpleTemplate("./tpl/index.tpl");
+		$index->set("header", $header);
+		$index->set("footer", $footer);
 	}
 	public function put(){
 
