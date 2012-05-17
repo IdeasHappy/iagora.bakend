6003e04468c5cdef353e438cdf7fbb67ef64f2db
Jesus Christian Cruz Acono <jccruz@openintelligence.mx>
2012-05-17 03:16:59 -0500

    changes to render

--- modules/escuelas.php
+++ modules/escuelas.php
@@ -1,12 +1,12 @@
 <?php
 class action extends actions{
 	public function index(){
-		return array(array("name" => 25),array("name" => 25));
+		$this->values = array(array("name" => 25),array("name" => 25));
 	}
 	public function add(){
-		return array(array("name" => 25));
+		$this->values = array(array("name" => 25));
 	}
 	public function get($id){
-		return array(array("name" => 25));
+		$this->values = array(array("name" => 25));
 	}
 }
