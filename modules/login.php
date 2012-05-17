04470762462df5c6b8446dd9c1aefea5c3ab2be5
Jesus Christian Cruz Acono <jccruz@openintelligence.mx>
2012-05-17 04:34:49 -0500

    fixed login

--- modules/login.php
+++ modules/login.php
@@ -1,7 +1,6 @@
 <?php
 class action extends actions{
 	public function form(){
-		echo "basura";
 		$this->values = array(array("name" => "write name"),array("password" => "write password"));
 	}
 	public function validate(){
